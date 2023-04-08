b155bd6f5651f300aeef5d9da6e91b3e8a65d979
JorgeGzz <91845721+Jorgegzz@users.noreply.github.com>
2023-04-08 13:00:20 -0600

    More Lobby GUI (Still in progress)

--- LobbySystem.cs
+++ LobbySystem.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using HarmonyLib;
+using Steamworks;
 using UnityEngine;
 
 namespace PoPM;
@@ -54,9 +56,15 @@ public class MainMenuRestartPatch
 public class LobbySystem: MonoBehaviour
 {
     public static LobbySystem instance;
+    public Stack<string> GUIStack = new();
+    public string JoinLobbyID = string.Empty;
+    public CSteamID ActualLobbyID = CSteamID.Nil;
     public bool isPauseMenu;
     public bool isInGame;
     public bool isGameLoaded;
+    public bool inLobby;
+    public bool isLobbyOwner;
+    
     
 
     private void Awake()
@@ -64,47 +72,130 @@ public class LobbySystem: MonoBehaviour
         instance = this;
     }
 
+    private void Start()
+    {
+        Callback<LobbyEnter_t>.Create(OnLobbyEnter);
+    }
+
     private void Update()
     {
-        //
+        if (Input.GetKeyDown(KeyCode.M) && !inLobby)
+        {
+            if (GUIStack.Count == 0)
+                GUIStack.Push("Main");
+            else
+                GUIStack.Clear();
+        }
     }
 
     private void OnGUI()
     {
         var lobbyStyle = new GUIStyle(GUI.skin.box);
 
-        if (!isInGame && isGameLoaded)
+        if (!isInGame && isGameLoaded && GUIStack.Count != 0)
         {
             GUILayout.BeginArea(new Rect((Screen.width - 220), (Screen.height - 550), 150f, 200f), string.Empty);
             GUILayout.BeginVertical(lobbyStyle);
+            switch (GUIStack.Peek())
+            {
+                case "Main":
+                {
+                    GUILayout.BeginHorizontal();
+                    GUILayout.FlexibleSpace();
+                    GUILayout.Label("<color=white>PoPM</color>");
+                    GUILayout.FlexibleSpace();
+                    GUILayout.EndHorizontal();
             
-            GUILayout.BeginHorizontal();
-            GUILayout.FlexibleSpace();
-            GUILayout.Label("<color=white>PoPM</color>");
-            GUILayout.FlexibleSpace();
-            GUILayout.EndHorizontal();
-            
-            GUILayout.Space(15f);
+                    GUILayout.Space(15f);
 
-            GUILayout.BeginHorizontal();
-            GUILayout.FlexibleSpace();
-            if (GUILayout.Button("JOIN"))
-               Plugin.Logger.LogInfo("Display join menu");
-            GUILayout.FlexibleSpace();
-            GUILayout.EndHorizontal();
+                    GUILayout.BeginHorizontal();
+                    GUILayout.FlexibleSpace();
+                    if (GUILayout.Button("JOIN"))
+                        GUIStack.Push("Join");
+                    GUILayout.FlexibleSpace();
+                    GUILayout.EndHorizontal();
             
-            GUILayout.Space(5f);
+                    GUILayout.Space(5f);
             
-            GUILayout.BeginHorizontal();
-            GUILayout.FlexibleSpace();
-            if (GUILayout.Button("HOST"))
-                Plugin.Logger.LogInfo("Display host menu");
-            GUILayout.FlexibleSpace();
-            GUILayout.EndHorizontal();
-            
-            
+                    GUILayout.BeginHorizontal();
+                    GUILayout.FlexibleSpace();
+                    if (GUILayout.Button("HOST"))
+                        GUIStack.Push("Host");
+                    GUILayout.FlexibleSpace();
+                    GUILayout.EndHorizontal();
+                    break;
+                }
+                case "Host":
+                {
+                    if (!inLobby)
+                    {
+                        SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypePrivate, 4);
+                        inLobby = true;
+                        isLobbyOwner = true;
+                    }
+                    
+                    GUILayout.BeginHorizontal();
+                    GUILayout.FlexibleSpace();
+                    GUILayout.Label($"HOST");
+                    GUILayout.FlexibleSpace();
+                    GUILayout.EndHorizontal();
+                    
+                    GUILayout.Space(5f);
+
+                    GUILayout.BeginHorizontal();
+                    GUILayout.FlexibleSpace();
+                    GUILayout.Label($"Lobby ID: {ActualLobbyID.GetAccountID().ToString()}");
+                    GUILayout.FlexibleSpace();
+                    GUILayout.EndHorizontal();
+                    
+                    GUILayout.Space(15f);
+                    
+                    if (GUILayout.Button("<color=#ed0e0e>EXIT</color>"))
+                    {
+                        ExitLobby();
+                        GUIStack.Pop();
+                    }
+
+                    break;
+                }
+                case "Join":
+                {
+                    GUILayout.BeginHorizontal();
+                    GUILayout.FlexibleSpace();
+                    GUILayout.Label($"JOIN");
+                    GUILayout.FlexibleSpace();
+                    GUILayout.EndHorizontal();
+                
+                    GUILayout.Space(15f);
+                
+                    JoinLobbyID = GUILayout.TextField(JoinLobbyID);
+                
+                    GUILayout.Space(5f);
+                
+                    if (GUILayout.Button("JOIN"))
+                        throw new NotImplementedException();
+                
+                    if (GUILayout.Button("<color=#888888>BACK</color>"))
+                        GUIStack.Pop();
+                    break;
+                }
+            }
             GUILayout.EndVertical();
             GUILayout.EndArea();
         }
+    }
+
+    private void ExitLobby()
+    {
+        inLobby = false;
+        isLobbyOwner = false;
+        Plugin.Logger.LogInfo($"Leaving lobby! {ActualLobbyID.GetAccountID().ToString()}");
+        SteamMatchmaking.LeaveLobby(ActualLobbyID);
+    }
+
+    private void OnLobbyEnter(LobbyEnter_t pCallback)
+    {
+        ActualLobbyID = new CSteamID(pCallback.m_ulSteamIDLobby);
+        Plugin.Logger.LogInfo($"Joined lobby! {ActualLobbyID.GetAccountID().ToString()}");
     }
 }
