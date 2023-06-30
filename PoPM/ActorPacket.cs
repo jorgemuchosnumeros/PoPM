83f476be07f9e1f6e059a67b719946d8a9c4314a
radsi <78619513+radsi@users.noreply.github.com>
2023-06-30 07:59:39 +0200

    Randomized Players Skin ✔

--- PoPM/ActorPacket.cs
+++ PoPM/ActorPacket.cs
@@ -10,7 +10,10 @@ namespace PoPM
         public Vector3 FacingDirection;
 
         public ActorStateFlags Flags;
+
         public int ID;
+
+        public string JSONSkin;
 
         public string Name;
 
@@ -24,5 +27,21 @@ namespace PoPM
         public bool Disconnected;
 
         public bool Dead;
+    }
+
+    public class CustomVillager
+    {
+        static System.Random random = new();
+
+        public bool Bottom1 { get; set; } = random.NextDouble() >= 0.5;
+        public bool Bottom2 { get; set; } = random.NextDouble() >= 0.5;
+        public bool Top1 { get; set; } = random.NextDouble() >= 0.5;
+        public bool Top2 { get; set; } = random.NextDouble() >= 0.5;
+        public bool LaurelCrown { get; set; } = random.NextDouble() >= 0.5;
+        public bool ChinBeard { get; set; } = random.NextDouble() >= 0.5;
+        public int Hair { get; set; } = random.Next(0, 18);
+        public int HairColor { get; set; } = random.Next(0, 5);
+        public int Gender { get; set; } = random.Next(0, 2);
+        public int SkinColor { get; set; } = random.Next(0, 4);
     }
 }
